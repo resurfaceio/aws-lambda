0aa784138b87331845816dce81764ff2dc80b78d
Ramón Márquez <ramn.marquez@gmail.com>
2022-05-13 17:22:09 -0400

    feat: add resurfaceio-logger

--- index.js
+++ index.js
@@ -1,44 +1,46 @@
-const http = require('http');
-const urls = require('url');
+const { URL } = require('url');
+const {
+    HttpLogger,
+    HttpMessage,
+    HttpRequestImpl,
+    HttpResponseImpl
+} = require('resurfaceio-logger');
 
-const USAGE_LOGGERS_URL = "PASTE URL HERE"
-const target = urls.parse(USAGE_LOGGERS_URL);
-const url_options = {
-    host: target.host.split(':')[0],
-    port: target.port,
-    path: target.path,
-    method: 'POST',
-    headers: {
-        'Content-Encoding': 'identity',
-        'Content-Type': 'application/json; charset=UTF-8',
-        'User-Agent': `Resurface/0.1 (AWS Lambda)`
-    }
-};
+let logger = new HttpLogger({rules: 'include debug'});
 
 exports.handler = async (event) => {
-    // TODO implement
-    const msg = { evt: event };
-    const response = {
-        statusCode: 200,
-        message: ""
-    };
-    return new Promise((resolve) => {
-        try {
-            const request = http.request(url_options, (resp) => {
-                response.message = {
-                    status: resp.statusCode,
-                    content: msg
-                };
-                resolve(JSON.stringify(response));
-            });
-            request.on('error', (err) => {
-                response.message = err;
-                resolve(JSON.stringify(response));
-            });
-            request.write(JSON.stringify(msg));
-            request.end();
-        } catch (e) {
-            resolve(e);
-        }
-    });
+    const request = new HttpRequestImpl();
+    const response = new HttpResponseImpl();
+    const url = new URL(event.nativeURL);
+    
+    request.hostname = url.hostname;
+    request.protocol = url.protocol;
+    request.url = url.pathname;
+    
+    request.method = event.nativeHttpMethod;
+    
+    for (const key of Object.keys(event.requestHeaders)) {
+        request.addHeader(key, event.requestHeaders[key]);
+    }
+    
+    for (const key of Object.keys(event.queryParameters)) {
+        request.addQueryParam(key, event.queryParameters[key]);
+    }
+    
+    response.statusCode = event.responseCode;
+    
+    for (const key of Object.keys(event.nativeResponseHeaders)) {
+        response.addHeader(key, event.nativeResponseHeaders[key]);
+    }
+    const now = Date.now().toString();
+    const interval = event.providerTime;
+    return HttpMessage.send(
+        logger,
+        request,
+        response,
+        event.nativeResponsePayload,
+        event.nativeRequestPayload,
+        now,
+        interval
+    );    
 };
