9935677fa5329b31f3243dac69eb030be4e4de4b
Ramón Márquez <ramn.marquez@gmail.com>
2022-05-13 18:07:38 -0400

    feat: add support for Software AG API management platform

--- index.js
+++ index.js
@@ -1,46 +1,19 @@
-const { URL } = require('url');
-const {
-    HttpLogger,
-    HttpMessage,
-    HttpRequestImpl,
-    HttpResponseImpl
-} = require('resurfaceio-logger');
+const { HttpLogger, HttpMessage } = require('resurfaceio-logger');
+const { processEvent } = require('./lib/softwareag')
 
 let logger = new HttpLogger({rules: 'include debug'});
 
 exports.handler = async (event) => {
-    const request = new HttpRequestImpl();
-    const response = new HttpResponseImpl();
-    const url = new URL(event.nativeURL);
-    
-    request.hostname = url.hostname;
-    request.protocol = url.protocol;
-    request.url = url.pathname;
-    
-    request.method = event.nativeHttpMethod;
-    
-    for (const key of Object.keys(event.requestHeaders)) {
-        request.addHeader(key, event.requestHeaders[key]);
-    }
-    
-    for (const key of Object.keys(event.queryParameters)) {
-        request.addQueryParam(key, event.queryParameters[key]);
-    }
-    
-    response.statusCode = event.responseCode;
-    
-    for (const key of Object.keys(event.nativeResponseHeaders)) {
-        response.addHeader(key, event.nativeResponseHeaders[key]);
-    }
     const now = Date.now().toString();
-    const interval = event.providerTime;
+    const call = processEvent(event);
+    
     return HttpMessage.send(
         logger,
-        request,
-        response,
-        event.nativeResponsePayload,
-        event.nativeRequestPayload,
+        call.request,
+        call.response,
+        call.response_body,
+        call.request_body,
         now,
-        interval
+        call.interval
     );    
 };
