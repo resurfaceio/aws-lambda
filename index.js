c2c865f5fd0948c819235e4640cc58ec76d2fa81
Ramón Márquez <ramn.marquez@gmail.com>
2022-05-16 11:46:33 -0400

    feat: add support for custom env vars for rules and overflow cap

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
 const { HttpLogger, HttpMessage } = require('resurfaceio-logger');
 const { processEvent } = require('./lib/softwareag')
 
-let logger = new HttpLogger({rules: 'include debug'});
+const RESPONSE_BODY_LIMIT = 1024 * 1024;
+let environment = {
+    rules: process.env.USAGE_LOGGERS_RULES,
+    body_limit: process.env.USAGE_LOGGERS_LIMIT || RESPONSE_BODY_LIMIT,
+    queue: process.env.USAGE_LOGGERS_QUEUE
+};
+let logger = new HttpLogger(environment);
 
 exports.handler = async (event) => {
     const now = Date.now().toString();
     const call = processEvent(event);
-    
+    const response_body = call.response_body;
+    if (response_body.length > environment.body_limit) {
+        response_body = `{"overflowed: ${response_body.length}"}`;
+    }
+
     return HttpMessage.send(
         logger,
         call.request,
         call.response,
-        call.response_body,
+        response_body,
         call.request_body,
         now,
         call.interval
